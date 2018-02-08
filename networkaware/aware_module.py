c014a34d9f19d3a2f4c0211aaf7b9ab09de6dbbc
xiaolunzi <nigulasikaochuan@sjtu.edu.cn>
2018-02-08 16:48:00 +0800

    simple_sample

--- networkaware/aware_module.py
+++ networkaware/aware_module.py
@@ -1,12 +1,14 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, set_ev_cls
+from ryu.controller.handler import CONFIG_DISPATCHER, set_ev_cls, MAIN_DISPATCHER
 from ryu.lib import hub
+from ryu.lib.packet import packet, arp
 from ryu.topology import api
 from ryu.topology import event
 from ryu.ofproto import ofproto_v1_3
 
 from common_function import add_miss_flow
+from common_function import _build_packet_out
 
 EventToListened = [event.EventSwitchEnter, event.EventSwitchLeave, event.EventSwitchReconnected,
                    event.EventLinkAdd,
@@ -22,9 +24,11 @@ class Aware(app_manager.RyuApp):
         super(Aware, self).__init__(*args, **kwargs)
 
         # self.logger.info(self.sw)
+        self.datapaths = {}  # dpid---->datapath
         self.name = "aware"
         self.switches = []
-        self.port_of_switches = {}  # (sw.id)--->port_np
+        self.port_of_switches = {}  # (sw.id)--->set(port_np)
+        self.port_of_switches_remained = {}  # {dpid}--->set(port_no)
         self.LinkBetweenSwitches = {}  # (src.id,dst.id)----->src_port_no,dst_port_no
         self.monitor = hub.spawn(self._monitor)
         self.HostSwitches = {}  # (sw.dpid,port)---->host_ip,host_mac
@@ -39,6 +43,7 @@ class Aware(app_manager.RyuApp):
         msg = ev.msg
         datapath = msg.datapath
         self.logger.info("switch {} is connected".format(datapath.id))
+        self.datapaths[datapath.id] = datapath
         add_miss_flow(datapath)
 
     def _monitor(self):
@@ -50,25 +55,50 @@ class Aware(app_manager.RyuApp):
             hub.sleep(1)
             i += 1
 
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_hanler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        parser = datapath.ofproto_parser
+        pkt = packet.Packet(msg.data)
+        arp_packet = pkt.get_protocol(arp.arp)
+
+        if arp_packet:
+
+            dst_ip = arp_packet.dst_ip
+            dst_mac = arp_packet.dst_mac
+            self.logger.info("交换机{}发出arp，目的ip{},目的mac{}".format(datapath.id, dst_ip, dst_mac))
+            # self.logger.info("已知的主机列表为{}".format(self.HostSwitches))
+            for (sw, sw_port), (host_ip, host_mac) in self.HostSwitches.items():
+                # self.logger.info("for ")
+                if host_ip == dst_ip:
+                    self.arp_forward(sw, sw_port, msg)
+                    break
+            # 正常循环结束了，都没进行arp转发，说明目标主机不可知，将arp包发送的交换机的所有剩余端口
+            else:
+
+                self.flood_arp(msg)
+
     @set_ev_cls(EventToListened)
     def topo(self, ev):
-        self.getSwitch()
-        self.getPortOfSwitches()
-        self.getLinkBetweenSwitches()
-        self.getHosts()
+        self.get_switch()
+        self.get_port_of_switches()  # dpid---->set
+        self.get_link_between_switches()
+        self.get_port_of_switches_remained()
+        self.get_hosts()
         # self.logger.info(self.switches)
-        # self.logger.info(self.LinkBetweenSwitches)
-        # self.logger.info(self.HostSwitches)
+        # self.logger.info("{}".format(self.LinkBetweenSwitches))
+        # # self.logger.info(self.HostSwitches)
+        # self.logger.info("{}".format(self.port_of_switches_remained))
 
-
-    def getSwitch(self):
+    def get_switch(self):
         '''
 
         :return:switches:dp,port  port:dpid,port_no
         '''
         self.switches = api.get_all_switch(self)
 
-    def getPortOfSwitches(self):
+    def get_port_of_switches(self):
         for sw in self.switches:
             datapath = sw.dp
             id = datapath.id
@@ -77,7 +107,7 @@ class Aware(app_manager.RyuApp):
             for port in sw.ports:
                 self.port_of_switches[id].add(port.port_no)
 
-    def getLinkBetweenSwitches(self):
+    def get_link_between_switches(self):
         AllLink = api.get_all_link(self)
         for Link in AllLink:
             src_port = Link.src
@@ -86,11 +116,43 @@ class Aware(app_manager.RyuApp):
             dst_sw_dpid = dst_port.dpid
             self.LinkBetweenSwitches[(src_sw_dpid, dst_sw_dpid)] = (src_port.port_no, dst_port.port_no)
 
-    def getHosts(self):
+    def get_hosts(self):
         hosts = api.get_host(self)
 
         for host in hosts:
-            self.HostSwitches[(host.port.dpid, host.port.port_no)] = (host.ipv4, host.mac)
-        #self.logger.info(self.HostSwitches)
+            # self.logger.info("get hosts中，主机的ipv4{}".format(host.ipv4))
+            self.HostSwitches[(host.port.dpid, host.port.port_no)] = (host.ipv4[0], host.mac)
+        # self.logger.info(self.HostSwitches)
 
+    def get_port_of_switches_remained(self):
+        for sw in self.switches:
+            dpid = sw.dp.id
+            self.port_of_switches_remained.setdefault(dpid, self.port_of_switches[dpid])
+            for key in self.LinkBetweenSwitches:
+                if key[0] == dpid:
+                    # self.logger.info("交换机{}所有的端口是{}".format(dpid,self.port_of_switches[dpid]))
+                    self.port_of_switches_remained[dpid] = self.port_of_switches_remained[dpid] - {
+                        self.LinkBetweenSwitches[key][0]}
+                    # self.logger.info("交换机{}去掉的端口是{}".format(dpid,{self.LinkBetweenSwitches[key][0]}))
+                    # self.logger.info("交换机{}剩余{}".format(id,self.port_of_switches_remained[dpid]))
 
+    def arp_forward(self, sw, sw_port, msg):
+        datapath = self.datapaths[sw]
+        # ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        self.logger.info("arp forward 发送到交换机{}的端口{}".format(datapath.id,sw_port))
+        actions = [parser.OFPActionOutput(sw_port)]
+        arp_message = _build_packet_out(datapath, actions, msg.data)
+        datapath.send_msg(arp_message)
+
+    def flood_arp(self, msg):
+        # 发送packet_in 的交换机
+        # parser = datapath.ofproto_parser
+        self.logger.info("flooding arp message")
+        for dpid, ports in self.port_of_switches_remained.items():
+            datapath = self.datapaths[dpid]
+            parser = datapath.ofproto_parser
+            for port in ports:
+                actions = [parser.OFPActionOutput(port)]
+                packet_arp = _build_packet_out(datapath, actions, msg.data)
+                datapath.send_msg(packet_arp)
--- networkaware/common_function.py
+++ networkaware/common_function.py
@@ -1,16 +1,28 @@
 def add_miss_flow(datapath):
-        '''
 
-        :param datapath:添加miss-flow流表的函数
-        :return: None
-        '''
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        actions = [parser.OFPActionOutput(port=ofproto.OFPP_CONTROLLER, max_len=ofproto.OFPCML_MAX)]
-        send_flow_mod(datapath, actions, pri=0, match=parser.OFPMatch())
+    ofproto = datapath.ofproto
+    parser = datapath.ofproto_parser
+    actions = [parser.OFPActionOutput(port=ofproto.OFPP_CONTROLLER, max_len=ofproto.OFPCML_MAX)]
+    send_flow_mod(datapath, actions, pri=0, match=parser.OFPMatch())
 
 
 
+
+def _build_packet_out(datapath, actions, data):
+    '''
+
+          :param datapath:这个packout消息要发送到的交换机
+          :param actions: 动作必须是可迭代的
+          :param data: packetout消息要封装的数据
+          :return: 无
+    '''
+
+    ofproto = datapath.ofproto
+    parser = datapath.ofproto_parser
+        # actions = [parser.OFPActionOutput(port)]
+    packet_out_msg = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER,
+                                             in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=data)
+    return packet_out_msg
 def send_flow_mod(datapath, actions, pri, match, idle_time=0, hard_time=0):
     '''
     下发正常流表的函数
@@ -28,7 +40,6 @@ def send_flow_mod(datapath, actions, pri, match, idle_time=0, hard_time=0):
     parser = datapath.ofproto_parser
     ofproto = datapath.ofproto
     ins = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-    message = parser.OFPFlowMod(datapath=datapath, command=ofproto.OFPFC_ADD, priority=pri,
-                                    match=match, idle_timeout=idle_time, hard_timeout=hard_time, instructions=ins)
+    message = parser.OFPFlowMod(datapath=datapath, command=ofproto.OFPFC_ADD, priority=pri,match=match, idle_timeout=idle_time, hard_timeout=hard_time, instructions=ins)
 
     datapath.send_msg(message)
